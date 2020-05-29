e467bc029bb3b1b267201e1349c17c8fc5fbe198
Al Graham <agraham@redhat.com>
2020-05-29 16:03:47 +0100

    testing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const server_port = process.env.PORT || 8080;
-const server_ip_address = process.env.IP || '0.0.0.0';
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 // Pull in our Quotes file
 const quotes = require('./data/quotes.json');
 // Require the framework and instantiate it
 const fastify = require('fastify')({
-    logger: false
+    logger: true
 });
+console.log("hello");
 
 // Basic quote route
 fastify.get('/quote', function(request, reply) {
@@ -14,20 +15,20 @@ fastify.get('/quote', function(request, reply) {
 });
 
 // Get quote for specific character
-fastify.get('/quote/:character', function(request, reply) {
-    const character = request.params.character.replace(/%20/g, " ");
-    console.log(character);
-    const charArray = quotes.filter(function(quote) {
-        return quote.character.toUpperCase() === character.toUpperCase()
-    });
-    if (charArray.length === 0) {
-    	return reply.code(404).type('text/html').send('Not Found');
-    }
-    return reply.send(JSON.stringify(charArray[Math.floor(Math.random() * charArray.length)]));
-});
+// fastify.get('/quote/:character', function(request, reply) {
+//     const character = request.params.character.replace(/%20/g, " ");
+//     console.log(character);
+//     const charArray = quotes.filter(function(quote) {
+//         return quote.character.toUpperCase() === character.toUpperCase()
+//     });
+//     if (charArray.length === 0) {
+//     	return reply.code(404).type('text/html').send('Not Found');
+//     }
+//     return reply.send(JSON.stringify(charArray[Math.floor(Math.random() * charArray.length)]));
+// });
 
 // Run the server
-fastify.listen(3000, '0.0.0.0', function(err, address) {
+fastify.listen(server_port, server_ip_address, function(err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
