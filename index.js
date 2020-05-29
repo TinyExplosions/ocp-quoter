d5af6ac17d33eb958ed2641f9cd831ae761cbf03
Al Graham <agraham@redhat.com>
2020-05-29 16:10:22 +0100

    tweaking

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ fastify.get('/quote', function(request, reply) {
 // Get quote for specific character
 fastify.get('/quote/:character', function(request, reply) {
     const character = request.params.character.replace(/%20/g, " ");
-    console.log(character);
-    const charArray = quotes.filter(function(quote) {
-        return quote.character.toUpperCase() === character.toUpperCase()
-    });
-    if (charArray.length === 0) {
-    	return reply.code(404).type('text/html').send('Not Found');
+    let charArray;
+    if (character) {
+        charArray = quotes.filter(function(quote) {
+            return quote.character.toUpperCase() === character.toUpperCase()
+        });
+        if (charArray.length === 0) {
+            return reply.code(404).type('text/html').send('Not Found');
+        }
+    } else {
+        charArray = quotes;
     }
     return reply.send(JSON.stringify(charArray[Math.floor(Math.random() * charArray.length)]));
 });
